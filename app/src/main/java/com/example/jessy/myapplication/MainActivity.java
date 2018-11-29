15b8343f62ca9b2cd04ec7e13b937782985ee012
JessicaAraya <45077132+JessicaAraya@users.noreply.github.com>
2018-11-29 13:11:57 -0300

    menu
    Co-Authored-By: Cristian Andres Moreno Labadie <cristianlabadie@users.noreply.github.com>

---
+++ app/src/main/java/com/example/jessy/myapplication/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.jessy.myapplication;
+
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout nDrawerLayout;
+    private ActionBarDrawerToggle nToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nDrawerLayout= (DrawerLayout) findViewById(R.id.drawer);
+        nToggle = new ActionBarDrawerToggle(this, nDrawerLayout, R.string.Open,R.string.Close);
+        nDrawerLayout.addDrawerListener(nToggle);
+        nToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (nToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
